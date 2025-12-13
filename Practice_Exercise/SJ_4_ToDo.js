b13bb1bc42a87edbc42403568306327739aec23d
Sweta <swetajhaofficial11@gmail.com>
2025-12-13 13:23:47 +0530

    Initial commit

---
+++ Practice_Exercise/SJ_4_ToDo.js
@@ -0,0 +1,31 @@
+let todoList=[{item: '', dueDate:''}];
+displayItems();
+function addTodo(){
+    let inputElement =document.querySelector('#todo-input');
+    let dateElement =document.querySelector('#todo-date');
+    let todoItem= inputElement.value;
+    todoList.push(todoItem);
+    let todoDate = dateElement.value;
+    todoList.push({item: todoItem, dueDate:todoDate} );
+    inputElement.value='';
+    dateElement.value='';
+
+    displayItems();
+}
+
+    function displayItems(){
+    let containerElement= document.querySelector('.todo-container');
+    let newHtml ='';
+    for(let i=0; i<todoList.length;i++){
+        // let todoItem = todoList[i].item;
+        // let dueDate = todoList[i].dueDate;
+        let {item, dueDate}= todoList[i];
+        newHtml+= `
+        
+        <span>${item}</span>
+        <span>${dueDate}</span>
+        <button class=" todo-delete" onclick="todoList.splice(${i},1); displayItems();">Delete</button>
+        `;
+    }  
+    containerElement.innerHTML= newHtml;
+    }
